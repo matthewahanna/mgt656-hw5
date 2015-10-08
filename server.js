58d4d7d55ba579001952bbb4ad1842fb8561641e
matthewahanna <matthew.a.hanna@gmail.com>
2015-10-08 20:04:12 +0000

    Restored Hello functionality

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Still active.');
+    response.send('Hello MGT-656!');
 });
 
 app.get('/nickname', function(request, response){
